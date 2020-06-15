1ef98ef2abace7f04f972cf4d058c4fada26f48e
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-06-15 19:42:38 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -6,4 +6,13 @@ const server = express();
 
 server.use(express.json());
 
+//router work
+const accountRouter = require('../accounts/accountsRouter');
+server.use('/api/accounts', accountRouter)
+
+
+server.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
 module.exports = server;
